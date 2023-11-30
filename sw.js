fe6851f551ff75a1bf5492580d44997de35abadf
mArchilli <archillimatias@gmail.com>
2023-11-30 16:19:04 -0300

    feat: add POKE API

--- sw.js
+++ sw.js
@@ -7,12 +7,13 @@ self.addEventListener("install", (e) => {
     const cache = caches.open("mi-cache-2").then((cache) => {
         cache.addAll([
             '/',
+            '/index.html',
+            '/js/script.js',
             '/img/favicon.png',
             '/css/styles.css',
             'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
             'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
-            'https://rickandmortyapi.com/api/character/?status=alive&limit=20',
-            '/js/script.js',
+            'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20', 
         ]);
     });
     e.waitUntil(cache);
@@ -21,17 +22,17 @@ self.addEventListener("install", (e) => {
 self.addEventListener("fetch", (e) => {
     const url = e.request.url;
     console.log(url);
-    const response =
-        fetch(e.request)
-            .then((res) => {
-              return caches.open('mi-cache-2').then(cache => {
-                  cache.put(e.request, res.clone());
-                  return res;
-              })
-            })
-            .catch((err) => {
-                return caches.match(e.request);
-            })
-    e.respondWith(response);
 
+    if(url.includes("index.html")){
+        e.respondWith(
+            fetch(url)
+            .then(respuesta => {
+                if(respuesta.status == 404){
+                    return {}
+                } else {
+                    return respuesta;
+                }
+            })
+        )
+    }
 });
